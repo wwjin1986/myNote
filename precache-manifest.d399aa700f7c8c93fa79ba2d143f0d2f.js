968d7acf7098aea3a85e700818f1888d50fe1266
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-08-15 16:19:05 -0400

    Updates

---
+++ precache-manifest.d399aa700f7c8c93fa79ba2d143f0d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cbef74d84d7b5155e26c6b7f6bd287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d0895f54944114cba2",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fd4add9d0b7f0a2ff9f2",
+    "url": "/static/css/main.8f5e5055.chunk.css"
+  },
+  {
+    "revision": "20d0895f54944114cba2",
+    "url": "/static/js/2.5b65b9e9.chunk.js"
+  },
+  {
+    "revision": "fd4add9d0b7f0a2ff9f2",
+    "url": "/static/js/main.f3a3b461.chunk.js"
+  },
+  {
+    "revision": "c100e4e8654246e586fa",
+    "url": "/static/js/runtime~main.f9313cf9.js"
+  }
+]);
